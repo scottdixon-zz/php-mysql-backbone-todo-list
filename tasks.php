3dddf2f25ca38e338cd56fb5ebd5dc0e2df84862
jankergg <jankergg@jankergg-3.local>
2015-12-25 16:43:58 +0800

    add a default value to 'done' col

--- tasks.php
+++ tasks.php
@@ -1,8 +1,9 @@
 <?php
-
-	$db = new PDO("mysql:host=localhost;dbname=tasks", "root", "root");
+    // host must set by IP;or it will throw an error 'PDOException SQLSTATE[HY000] [2002]...'
+    // the 3rd parameter should be Password,i just ignore it cause i didnt set a password;
+	$db = new PDO("mysql:host=127.0.0.1;port=3306;dbname=tasks", "root");
 	$data = json_decode(file_get_contents('php://input'));
-
+    
 	if ($_SERVER['REQUEST_METHOD'] == "GET"){
 		$statement = $db->query('SELECT * FROM tasks');
 		$statement->setFetchMode(PDO::FETCH_ASSOC);
@@ -12,7 +13,7 @@
 	if ($_SERVER['REQUEST_METHOD'] == "POST"){
 		$sql = "INSERT INTO tasks (title) values (:title)";
 		$query = $db->prepare($sql);
-		$query->execute(array(":title"=>$data->title));
+		$query->execute(array(":title"=>$data->title));// here should set a default value for 'done',so i modified the sql sentence [tasks.sql,line 8]
 		$result['id'] = $db->lastInsertId();
 		echo json_encode($result);
 	}
--- tasks.sql
+++ tasks.sql
@@ -5,7 +5,7 @@
 CREATE TABLE `tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
-  `done` tinyint(1) NOT NULL,
+  `done` tinyint(1) default 0, -- here set a default value ,o 
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
@@ -13,6 +13,6 @@ CREATE TABLE `tasks` (
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `title`, `done`) VALUES
-(2, 'Example Task', 1),
+INSERT INTO `tasks` (`id`, `title`, `done`) VALUES 
+(2, 'Example Task', 1), 
 (3, 'A Completed Task', 1);
